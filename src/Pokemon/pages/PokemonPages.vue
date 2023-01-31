5af0f98b3f73b950d0886f43fdad5363c8e787ea
carlosmonroyg <basesdedatosucc1920@gmail.com>
2023-01-30 21:49:09 -0500

    ...

--- src/Pokemon/pages/PokemonPages.vue
+++ src/Pokemon/pages/PokemonPages.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>Pokemon Pagues</h1>
+    <h1>Pokemon Pagues <span>#{{ id }}</span></h1>
 </template>
+<script>
+export default {
+    props: {
+        id: {
+            type: Number,
+            requuired: true
+        }
+    },
+    data() {
+
+        return {
+            //id: null
+        }
+    },
+    created() {
+    //     const { id } = this.$route.params
+    //     console.log(id)
+    //    // this.id = id
+
+    }
+
+}
+</script>
